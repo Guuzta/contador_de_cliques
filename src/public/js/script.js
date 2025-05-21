0cb8b4a49b289feb8e31f10c972a2555b26fc1b8
Guuzta <Guuhsilva13@outlook.com>
2025-05-21 20:12:24 -0300

    adicionando eventos de clique, opcoes de tempo e visualizacao de cliques na pagina

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,10 +1,9 @@
 const circleAnimation = {
 
+
     init: function () {
         this.elementsSelector()
         this.events()
-        this.animations
-
     },
 
     elementsSelector: function () {
@@ -14,6 +13,7 @@ const circleAnimation = {
 
     events: function () {
         this.$circle.addEventListener('click', this.animations.rippleAnimation.bind(this))
+
     },
 
     animations: {
@@ -41,6 +41,8 @@ const circleAnimation = {
         },
 
         rippleAnimation: function (e) {
+
+            console.log(e.target.firstElementChild)
 
             this.animations.backgroundAnimation()
 
@@ -72,9 +74,179 @@ const circleAnimation = {
             self.$circleBackground.classList.add('rotate')
         },
 
+        resetAnimation: function () {
+            console.log('cu largo')
+        }
 
     }
+}
+const clickCounter = {
+
+
+
+    init: function () {
+        this.elementsSelector()
+        this.addEvents()
+    },
+
+    elementsSelector: function () {
+        this.$circle = document.getElementById('circle')
+        this.$time = document.getElementById('time')
+        this.$timers = document.querySelectorAll('.timer')
+        this.$previousScore = document.getElementById('previousScore')
+        this.$currentScore = document.getElementById('currentScore')
+    },
+
+    addEvents: function () {
+        const self = this
+
+        window.addEventListener('load', self.events.default_load.bind(self))
+
+        this.$timers.forEach(function (timer) {
+            timer.addEventListener('click', self.events.timer_click.bind(self))
+        })
+
+        this.$circle.addEventListener('click', this.events.scoreInfo.bind(this), { once: true })
+
+        this.$circle.addEventListener('click', this.events.circle_click.bind(this))
+
+    },
+
+    events: {
+
+        base: function () {
+            return clickCounter
+        },
+
+
+        default_load: function (e) {
+            const defaultTimer = this.$timers[0]
+            defaultTimer.click()
+        },
+
+        timer_click: function (e) {
+
+            const timer = e.target
+            const list = e.target.parentNode
+            const itens = Array.from(list.children)
+            const index = itens.indexOf(timer)
+
+            itens.forEach((item) => {
+                if (item.classList.contains('selected')) {
+                    item.classList.remove('selected')
+                }
+            })
+
+            timer.classList.add('selected')
+
+            this.seconds
+
+            switch (index) {
+                case 0:
+                    this.seconds = 15
+                    this.events.restartGame()
+                    break;
+                case 1:
+                    this.seconds = 20
+                    this.events.restartGame()
+                    break;
+                case 2:
+                    this.seconds = 30
+                    this.events.restartGame()
+                    break;
+            }
+
+            this.$time.innerHTML = `<span>${this.seconds}</span>`
+
+            this.milliseconds = this.seconds * 1000
+        },
+
+
+        circle_click: function (e) {
+
+            this.clicks++
+
+            this.$currentScore.innerHTML = `
+                <span>
+                    <p>Cliques</p>
+                    <p>${this.clicks}</p>
+                </span>            
+            `
+        },
+
+        scoreInfo: function () {
+
+            const self = this
+
+            setTimeout(function () {
+
+
+                Swal.fire({
+                    title: "Bom trabalho!",
+                    text: `${self.clicks} cliques em ${self.seconds} segundos`,
+                    icon: "success",
+                    confirmButtonText: "Salvar",
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    showDenyButton: true,
+                    denyButtonText: `Não salvar`
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        Swal.fire({
+                            icon: "success",
+                            title: "Seus pontos foram salvos!",
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            showCancelButton: false,
+                            showConfirmButton: false,
+                            showDenyButton: true,
+                            denyButtonText: "Fechar",
+
+                        });
+                    } else if (result.isDenied) {
+                        Swal.fire({
+                            icon: "error",
+                            title: "Seus pontos não foram salvos!",
+                            allowOutsideClick: false,
+                            allowEscapeKey: false,
+                            showCancelButton: false,
+                            showConfirmButton: false,
+                            showDenyButton: true,
+                            denyButtonText: "Fechar",
+
+                        });
+                    }
+                })
+
+                clearInterval(interval)
+
+            }, this.milliseconds)
+
+
+            this.timeLeft = this.milliseconds / 1000
+
+            const interval = setInterval(this.events.timeLeft.bind(this), 1000)
+
+            this.clicks = 0
+        },
+
+        timeLeft: function () {
+            this.timeLeft--
+            this.$time.innerHTML = `<span>${this.timeLeft}</span>`
+        },
+
+        saveData: function () {
+            
+        },
+
+        restartGame: function () {
+            circleAnimation.animations.resetAnimation()
+        }
+    }
+
 
 }
 
+
 circleAnimation.init()
+clickCounter.init()
